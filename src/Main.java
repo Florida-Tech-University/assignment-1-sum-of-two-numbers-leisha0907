e5ea98da028a2cae49047871d61d3337e65a7c37
Leisha Agrawal <agrawalleisha0907@gmail.com>
2026-01-20 16:42:42 +0530

    Read two integers and print their sum

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
-        // TODO: Read two integers
-      
+        System.out.println(a + b);    
 
-        // TODO: Print their sum  using println
-      
         scanner.close();
     }
 }
